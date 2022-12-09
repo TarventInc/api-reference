a0a34811e528408180fc98924562fc9e301acb3a
Derek J <derekj@ixfactor.com>
2022-12-08 20:52:29 -0700

    Path change

---
+++ src/_app/immutable/chunks/2-53317691.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_page-f2223e0e.js";export{e as shared};
---
+++ src/_app/immutable/chunks/4-91cdfffb.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-f6cb5962.js";import{default as t}from"../components/pages/queries/_query_/_page.svelte-b3a5a8e6.js";export{t as component,r as shared};
